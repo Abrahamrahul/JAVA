7624c145725f4829c331f9ff51a16dc57e34c3ed
Abrahamrahul <bbrahulbas@gmail.com>
2024-02-11 20:16:00 +0530

    initial

---
+++ 1.Basic/variables.java
@@ -0,0 +1,16 @@
+class variables
+{
+	public static void main(String args[])
+	{
+		String name="Sakthivel";
+		int age=21;
+		float percent=21.2f;
+		char gender='M';
+		boolean married=false;
+		System.out.println("Name 		: "+name);
+		System.out.println("Age  		: "+age);
+		System.out.println("Percent     : "+percent);
+		System.out.println("Gender      : "+gender);
+		System.out.println("Married     : "+married);
+	}
+}
